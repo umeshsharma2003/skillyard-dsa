26214570441f073a71a239d688994dfb7b00da58
umeshsharma2003 <umeshsharma8273852625@gmail.com>
2025-03-18 13:39:45 +0530

    first commit

---
+++ dsa.js
@@ -0,0 +1,57 @@
+// Task 1: Basic Loop Analysis
+// Title: Understanding Simple Loop Complexity
+// Description: Analyze the following loop and determine its time complexity.
+
+// question==1// for(let i = 0; i < n; i++) {
+//     console.log("my name is umesh lawaniya ")
+// }
+
+// // output =time complexity =o(n)
+
+
+// question==2
+// for(let i = 0; i < n; i+=2) {
+//     
+// }
+
+
+// time complexity==o(n)
+
+// Description: Analyze the time complexity of traversing two arrays of sizes n and m separately.
+
+//  question-3// let  a[n], b[m];
+// for(let i = 0; i < n; i++) {
+//     a[i] = i;
+// }
+// for(let i = 0; i < m; i++) {
+//     b[i] = m - i;
+// }
+
+
+// time complexity=o(n+m)
+
+
+// question==4
+// Task 4: Nested Loop for Two Arrays
+// Title: Nested Loop Complexity
+// Description: Analyze the time complexity of the following nested loop, where two arrays of sizes n and m are traversed.
+
+// for(let i = 0; i < n; i++) {
+//     for(let j = 0; j < m; j++) {
+//         console.log(umesh);
+//     }
+// }
+
+// time comlexity==o(n*n)
+
+
+// Task 5: Logarithmic Complexity
+// Title: Logarithmic Growth in Loops
+// Description: Analyze the time complexity of the following loop where i increases exponentially.
+
+// let c = 0;
+// for(let i = 1; i < n; i *= k) {
+//     console.log(umesh)
+// }
+// Explain why the time complexity is O(log_k(n)) and describe how the value of k influences the growth rate.
+// time complexity==o(log_k(n))
